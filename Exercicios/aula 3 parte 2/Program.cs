f9966c8225032da7b7cb17d5ba74cb487057aec0
bianca maccarini <bianca.maccarini@gmail.com>
2024-03-04 20:30:16 -0300

    aula 3 parte 2

---
+++ Exercicios/aula 3 parte 2/Program.cs
@@ -0,0 +1,12 @@
+﻿//subistituindo conteudo de string
+string nomeCompleto = "Bianca Maccarini"
+nomeCompleto = nomeCompleto.Replace("Maccarini", "sla");
+Console.WriteLine(nomeCompleto);
+
+//comparacao de strings
+boll isNomeEqual = (nomeCompleto == "Bianca Maccarini");
+
+bool isNomeEqual2 = string.Eqquals(nomeCompleto, "Bianca sla");
+
+Console.WriteLine($"primeiro resultado: {isNomeEqual}");
+Console.WriteLine($"segundo resultado: {isNomeEqual2}");
